4a37efcb9d61e11a36ef3ce42ddf4a8784d4fea3
DenkSchuldt <dschuldt@outlook.com>
2014-07-06 19:54:32 -0500

    Wah Wah effect

--- gui.m
+++ gui.m
@@ -1,29 +1,5 @@
 function varargout = gui(varargin)
 % GUI MATLAB code for gui.fig
-%      GUI, by itself, creates a new GUI or raises the existing
-%      singleton*.
-%
-%      H = GUI returns the handle to a new GUI or the handle to
-%      the existing singleton*.
-%
-%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in GUI.M with the given input arguments.
-%
-%      GUI('Property','Value',...) creates a new GUI or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before gui_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to gui_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help gui
-
-% Last Modified by GUIDE v2.5 06-Jul-2014 14:27:32
-
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
@@ -46,18 +22,16 @@ end
 
 % --- Executes just before gui is made visible.
 function gui_OpeningFcn(hObject, eventdata, handles, varargin)
-[audio,fs,N] = wavread('vozam');
-audio = audio';
+[sound,fs,N] = wavread(getappdata(0,'fullpath'));
+sound = sound';
 % Save global values
 handles.ispushed = 0;
-handles.audio = audio;
+handles.sound = sound;
 handles.fs = fs;
+handles.k = 1;
 handles.output = hObject;
 % Update handles structure
 guidata(hObject, handles);
-
-% UIWAIT makes gui wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
@@ -74,19 +48,19 @@ elseif option == 'radiobutton2'
     %--- Ardilla ---%
 elseif option == 'radiobutton3'    
     %--- Eco ---%
-    audio = handles.audio;
+    sound = handles.sound;
     fs = handles.fs;
     delay = 0.15;
     decay = 0.6;
     k = zeros(1,delay*fs+2);
     k(1) = decay;
     k(end) = 1-decay;
-    result = conv(audio,k);
+    result = conv(sound,k);
     handles.result = result;
     guidata(hObject,handles)
 elseif option == 'radiobutton4'
     %--- Reverberación ---%
-    audio = handles.audio;
+    sound = handles.sound;
     fs = handles.fs;
     k = zeros(1,fs*0.5);
     k(1) = 0.5;
@@ -94,13 +68,49 @@ elseif option == 'radiobutton4'
     k(0.1*fs+1) = 0.1;
     k(0.1*fs+1) = 0.05;
     k(0.1*fs+1) = 0.03;
-    result = conv(audio,k);
+    result = conv(sound,k);
     handles.result = result;
-    guidata(hObject,handles)
+    guidata(hObject,handles);
 elseif option == 'radiobutton5'
-    %--- Random ---%
+    %--- Vibrato---%
 elseif option == 'radiobutton6'
-    %--- Vibrato ---%
+    %--- Wah Wah ---%
+    % src: http://www.cs.cf.ac.uk/Dave/CM0268/PDF/10_CM0268_Audio_FX.pdf
+    sound = handles.sound;
+    Fs = handles.fs;
+    damp = 0.05; % damping factor
+    minf=500;
+    maxf=3000;
+    Fw = 2000; % wah frequency 
+    delta = Fw/Fs;
+    Fc = minf:delta:maxf; % triangle wave of centre frequency values
+    while(length(Fc) < length(sound) )
+        Fc= [ Fc (maxf:-delta:minf) ];
+        Fc= [ Fc (minf:delta:maxf) ];
+    end
+    Fc = Fc(1:length(sound));
+    F1 = 2*sin((pi*Fc(1))/Fs);
+    Q1 = 2*damp;                % Size of the pass bands
+    yh=zeros(size(sound));
+    yb=zeros(size(sound));
+    yl=zeros(size(sound));
+    yh(1) = sound(1);
+    yb(1) = F1*yh(1);
+    yl(1) = F1*yb(1);
+    for n=2:length(sound),
+        yh(n) = sound(n) - yl(n-1) - Q1*yb(n-1);
+        yb(n) = F1*yh(n) + yb(n-1);
+        yl(n) = F1*yb(n) + yl(n-1);
+        F1 = 2*sin((pi*Fc(n))/Fs);
+    end
+    %normalise
+    maxyb = max(abs(yb));
+    yb = yb/maxyb;
+    handles.result = yb;
+    guidata(hObject,handles);
+elseif option == 'radiobutton7'
+    handles.result = handles.sound;
+    guidata(hObject,handles);
 end
 
 
@@ -121,26 +131,46 @@ handles.ispushed = isPushed;
 guidata(hObject,handles)
 
 
-% --- Executes during object creation, after setting all properties.
-function pushbutton1_CreateFcn(hObject, eventdata, handles)
-
-
-% --- Executes during object deletion, before destroying properties.
-function pushbutton1_DeleteFcn(hObject, eventdata, handles)
-
-
 % --- Executes on button press in pushbutton2.
 function pushbutton2_Callback(hObject, eventdata, handles)
 %close
-clear playsnd
+clear playsnd;
+rmappdata(0,'fullpath');
 delete(handles.output);
 
 
 % --- Executes on button press in pushbutton3.
 function pushbutton3_Callback(hObject, eventdata, handles)
 %less
+k = handles.k;
+disp(k);
+sound = handles.sound;
+if k >= 0.2
+    k = k - 0.1;
+    output = conv(sound,k);
+    sound = output;
+end
+handles.sound = sound;
+handles.k = k;
+guidata(hObject,handles);
 
 
 % --- Executes on button press in pushbutton4.
 function pushbutton4_Callback(hObject, eventdata, handles)
 %more
+k = handles.k;
+disp(k);
+sound = handles.sound;
+if k <= 1.9
+    k = k + 0.1;
+    output = conv(sound,k);
+    sound = output;
+end
+handles.sound = sound;
+handles.k = k;
+guidata(hObject,handles);
+
+
+function uipanel1_CreateFcn(hObject, eventdata, handles)
+function pushbutton1_CreateFcn(hObject, eventdata, handles)
+function pushbutton1_DeleteFcn(hObject, eventdata, handles)
--- index.m
+++ index.m
@@ -1,29 +1,5 @@
 function varargout = index(varargin)
 % INDEX MATLAB code for index.fig
-%      INDEX, by itself, creates a new INDEX or raises the existing
-%      singleton*.
-%
-%      H = INDEX returns the handle to a new INDEX or the handle to
-%      the existing singleton*.
-%
-%      INDEX('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in INDEX.M with the given input arguments.
-%
-%      INDEX('Property','Value',...) creates a new INDEX or raises the
-%      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before index_OpeningFcn gets called.  An
-%      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to index_OpeningFcn via varargin.
-%
-%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
-%      instance to run (singleton)".
-%
-% See also: GUIDE, GUIDATA, GUIHANDLES
-
-% Edit the above text to modify the response to help index
-
-% Last Modified by GUIDE v2.5 06-Jul-2014 13:29:34
-
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
@@ -50,9 +26,6 @@ handles.output = hObject;
 handles.recording = 0;
 guidata(hObject, handles);
 
-% UIWAIT makes index wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
-
 
 % --- Outputs from this function are returned to the command line.
 function varargout = index_OutputFcn(hObject, eventdata, handles) 
@@ -63,27 +36,28 @@ varargout{1} = handles.output;
 function pushbutton1_Callback(hObject, eventdata, handles)
 recording = handles.recording;
 recording = ~recording;
-y = '';
-Fs = 11025;
+% Setting parameters
+Fs = 44100;
+nbits = 16;
+% Creating the audiorecorder object
+rec = audiorecorder(Fs, nbits, 1);
 if recording
     set(hObject,'String','Grabando','ForegroundColor','red');
-    y = wavrecord(5*Fs,Fs,'int16');
+    record(rec);
 else
     set(hObject,'String','Grabar audio','ForegroundColor','green');
-    wavplay(y,Fs);
+    stop(rec);
+    %setappdata(0,'fullpath',getaudiodata(arec));
+    sound(getaudiodata(rec),Fs);
+    %gui
 end
 handles.recording = recording;
-guidata(hObject,handles)
+guidata(hObject,handles);
 
 
 % --- Executes on button press in pushbutton2.
 function pushbutton2_Callback(hObject, eventdata, handles)
 [filename pathname] = uigetfile({'*.wav'},'File Selector');
 fullpath = strcat(pathname,filename);
-[audio,fs,N] = wavread(fullpath);
-audio = audio';
-sound(audio,fs);
-openfig('gui');
-
-
-
+setappdata(0,'fullpath',fullpath);
+gui
